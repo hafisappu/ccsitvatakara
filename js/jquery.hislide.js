8482412ac74418cb7cd9906ee4ec4a222c835a3a
Saad-vavat <saadvavat@gmail.com>
2022-05-31 22:25:34 +0530

    first commit

--- js/jquery.hislide.js
+++ js/jquery.hislide.js
@@ -132,7 +132,7 @@ window.onload = function() {
          $(".slider").owlCarousel({
            loop: true,
            autoplay: true,
-           autoplayTimeout: 2000, //2000ms = 2s;
+           autoplayTimeout: 10000, //2000ms = 2s;
            autoplayHoverPause: true,
          });
          
